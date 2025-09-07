9b02aead81a0b06e10c29a3f9b988564887a812e
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-07 18:21:03 +1200

    time

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "classes/parser.hpp"
 #include "classes/config.hpp"
 #include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char* argv[])
 {
@@ -17,6 +19,20 @@ int main(int argc, char* argv[])
     std::cout << "Testing parser .....\n";
     ph._test_parse_scfile();
     return 0;  
+  }
+
+  if (command_input[1] == "--test-metadata")
+  {
+    std::string file = command_input[2];
+    struct stat result;
+    
+    if (stat(file.c_str(), &result))
+    {
+      auto time = result.st_mtime;
+      std::cout << "Time last modified was: " << time << "\n";  
+    }
+    
+    return 0;
   }
   
   ph.parse_scfile();
