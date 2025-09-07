190459bb7e32c0a65566efefbb14ce1a39fa1eaa
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-07 13:07:16 +1200

    testing

--- main.cpp
+++ main.cpp
@@ -7,28 +7,11 @@ int main()
 {
   ConfigHandler ch;
 
-  ch.write_file("Johnny");
-  ch.write_files({ "James", "Bob", "Barry", "Luke" });
-  std::vector<std::string> files = ch.get_files();
+  ch.write_compiler_options("-L/etc/curlpp -lcurl -lcurlpp");
+  std::string compiler_options = ch.get_compiler_options();
+  std::cout << "Compiler options are '" << compiler_options << "'\n";
 
-  for (int i = 0; i < files.size(); i++)
-    std::cout << files[i] << ", ";
-
-  std::cout << "\b\b.\n";
-
-  ch.remove_files({ 0, 2, 3 });
-  files = ch.get_files();
-
-  for (int i = 0; i < files.size(); i++)
-    std::cout << files[i] << ", ";
-
-  std::cout << "\b\b.\n";
-
-  ch.write_cpp_mode(false);
-  int cpp_mode = ch.get_cpp_mode();
-  
-  if (cpp_mode == 0)
-    std::cout << "CPP mode is disabled.\n";
-  else
-    std::cout << "CPP mode is enabled.\n";
+  ch.clear_compiler_options();
+  compiler_options = ch.get_compiler_options();
+  std::cout << "Compiler options are '" << compiler_options << "'\n";
 }
