4c38998dc803f23281b52f20eb9a1822060302aa
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-07 12:45:50 +1200

    build

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+function build_test()
+{
+  mkdir ./debug
+  g++ -o ./debug/screate ./main.cpp ./classes/config.cpp ./classes/config.hpp && compiled=true
+  if [[ "$compiled" == "true" ]] && [[ "$1" != "" ]]; then
+    git add .
+    git commit -m "$1" && git push
+    exit 0
+  else
+    echo "Error: add commit message or fix code!"
+    exit 1
+  fi
+}
+
+function build_fin()
+{
+  mkdir ./release
+  g++ -o ./debug/screate ./main.cpp ./classes/config.cpp ./classes/config.hpp && compiled=true
+  if [[ "$compiled" == "true" ]] && [[ "$1" != "" ]]; then
+    git add .
+    git commit -m "$1" && git push
+    exit 0
+  else
+    echo "Error: add commit message or fix code!"
+    exit 1
+  fi
+}
+
+case $1 in
+release) build_fin $2 ;;
+debug) build_test $2 ;;
+esac
