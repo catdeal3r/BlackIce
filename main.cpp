2a9ca726fbd7ccbec7f74d1dc28a81d9b0c66b0a
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-07 13:08:11 +1200

    testing

--- main.cpp
+++ main.cpp
@@ -7,11 +7,6 @@ int main()
 {
   ConfigHandler ch;
 
-  ch.write_compiler_options("-L/etc/curlpp -lcurl -lcurlpp");
   std::string compiler_options = ch.get_compiler_options();
-  std::cout << "Compiler options are '" << compiler_options << "'\n";
-
-  ch.clear_compiler_options();
-  compiler_options = ch.get_compiler_options();
   std::cout << "Compiler options are '" << compiler_options << "'\n";
 }
