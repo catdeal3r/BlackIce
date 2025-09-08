03b48fedf008641b7c4c2c036fcd764fc10e7f06
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-08 16:26:46 +1200

    fix?

--- main.cpp
+++ main.cpp
@@ -8,31 +8,32 @@ int main(int argc, char* argv[])
 {
   std::vector<std::string> command_input;
   
-  if (argc > 1)
-  {
-    for (int i = 0; i < argc; ++i)
-      command_input.push_back(argv[i]);
-  }
-  
   ParsingHandler ph;
   CompileHandler ch;
-
-  if (command_input[1] == "--test-parse")
+  
+  if (argc > 1)
   {
-    std::cout << "Testing parser .....\n";
-    ph._test_parse_scfile();
-    return 0;  
-  }
+    for (int i = 0; i < argc; i++)
+      command_input.push_back(argv[i]);
+  
 
-  if (command_input[1] == "--test-metadata")
-  {
-    std::string file = command_input[2];
-    std::cout << "Testing metadata for " << file << " .....\n";
+    if (command_input[1] == "--test-parse")
+    {
+      std::cout << "Testing parser .....\n";
+      ph._test_parse_scfile();
+      return 0;  
+    }
+
+    if (command_input[1] == "--test-metadata")
+    {
+      std::string file = command_input[2];
+      std::cout << "Testing metadata for " << file << " .....\n";
     
-    std::string time = ch.get_time_metadata_file(file); 
-    std::cout << "File write time is " << time << "\n";    
+      std::string time = ch.get_time_metadata_file(file); 
+      std::cout << "File write time is " << time << "\n";    
 
-    return 0;
+      return 0;
+    }
   }
   
   ph.parse_scfile();
