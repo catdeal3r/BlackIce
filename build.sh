273a4458aa7101484952a4b93acbf0f9be817726
dealerofallthecats <xhackerqwertyx@protonmail.com>
2025-09-21 14:15:08 +1200

    conceiseness

--- build.sh
+++ build.sh
@@ -1,34 +1,13 @@
 #!/bin/sh
 
-function build_test()
-{
-  mkdir ./debug
-  g++ -o ./debug/scr ./main.cpp ./classes/config.cpp ./classes/config.hpp ./classes/parser.hpp ./classes/parser.cpp ./classes/compiler.hpp ./classes/compiler.cpp && compiled=true
-  if [[ "$compiled" == "true" ]] && [[ "$1" != "" ]]; then
-    git add .
-    git commit -m "$1" && git push
-    exit 0
-  else
-    echo "Error: add commit message or fix code!"
-    exit 1
-  fi
-}
+mkdir ./debug
+g++ -o ./debug/scr ./main.cpp ./classes/config.cpp ./classes/config.hpp ./classes/parser.hpp ./classes/parser.cpp ./classes/compiler.hpp ./classes/compiler.cpp && compiled=true
+if [[ "$compiled" == "true" ]] && [[ "$1" != "" ]]; then
+  git add .
+  git commit -m "$1" && git push
+  exit 0
+else
+  echo "Error: add commit message or fix code!"
+  exit 1
+fi
 
-function build_fin()
-{
-  mkdir ./release
-  g++ -o ./release/scr ./main.cpp ./classes/config.cpp ./classes/config.hpp ./classes/parser.hpp ./classes/parser.cpp ./classes/compiler.hpp ./classes/compiler.cpp && compiled=true
-  if [[ "$compiled" == "true" ]] && [[ "$1" != "" ]]; then
-    git add .
-    git commit -m "$1" && git push
-    exit 0
-  else
-    echo "Error: add commit message or fix code!"
-    exit 1
-  fi
-}
-
-case $1 in
-release) build_fin $2 ;;
-debug) build_test $2 ;;
-esac
